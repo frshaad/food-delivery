906544bbf093e7adadb5b422457c18dfa0b59e6e
Farshad Hatami <farshad.hatami95@gmail.com>
2023-12-17 19:38:11 +0330

    update favicon

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,10 +1,21 @@
+import { Github } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+
 import ThemeSwitch from './ThemeSwitch';
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between py-6">
       <div></div>
-      <ThemeSwitch />
+      <div className="flex items-center gap-2">
+        <Button variant="outline" size="icon">
+          <a href="https://github.com/frshaad/food-delivery">
+            <Github />
+          </a>
+        </Button>
+        <ThemeSwitch />
+      </div>
     </header>
   );
 }
